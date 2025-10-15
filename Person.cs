fcc04d49affab5e469f6502b0ae9b1957857ddf5
RR75-CODE <ryanradieddine@gmail.com>
2025-10-15 14:43:44 -0400

    Fix formatting of Person class properties
    Formatted properties for consistency.

--- Person.cs
+++ Person.cs
@@ -4,10 +4,10 @@ namespace grading
 {
     public abstract class Person
     {
-        public string Name { get; protected set; }
-        public int Age { get; protected set; }
-        public DateTime DateOfBirth { get; protected set; }
-        public string Gender { get; protected set; }
+        public string Name {get; protected set;}
+        public int Age {get; protected set;}
+        public DateTime DateOfBirth {get; protected set;}
+        public string Gender {get; protected set;}
 
         protected Person(string name, int age, DateTime dob, string gender)
         {
